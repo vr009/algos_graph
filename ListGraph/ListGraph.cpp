2a5a0f9ca91f64661de1e741cc26bccaab263459
vr009 <slavarianov@yandex.ru>
2021-05-28 00:11:48 +0300

    ListGraph init

---
+++ ListGraph/ListGraph.cpp
@@ -0,0 +1,5 @@
+//
+// Created by slava on 28.05.2021.
+//
+
+#include "ListGraph.h"
---
+++ ListGraph/ListGraph.h
@@ -0,0 +1,14 @@
+//
+// Created by slava on 28.05.2021.
+//
+
+#ifndef ALGOS_GRAPH_LISTGRAPH_H
+#define ALGOS_GRAPH_LISTGRAPH_H
+
+
+class ListGraph {
+
+};
+
+
+#endif //ALGOS_GRAPH_LISTGRAPH_H
