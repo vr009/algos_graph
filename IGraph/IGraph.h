2e2b6c924cfd586c017d4d5e511e4cb5e3ac8bb2
vr009 <slavarianov@yandex.ru>
2021-05-28 00:10:46 +0300

    Interface added

---
+++ IGraph/IGraph.h
@@ -0,0 +1,16 @@
+#ifndef ALGOS_GRAPH_IGRAPH_H
+#define ALGOS_GRAPH_IGRAPH_H
+
+struct IGraph {
+    virtual ~IGraph() {}
+
+    // Добавление ребра от from к to.
+    virtual void AddEdge(int from, int to) = 0;
+
+    virtual int VerticesCount() const  = 0;
+
+    virtual std::vector<int> GetNextVertices(int vertex) const = 0;
+    virtual std::vector<int> GetPrevVertices(int vertex) const = 0;
+};
+
+#endif //ALGOS_GRAPH_IGRAPH_H
